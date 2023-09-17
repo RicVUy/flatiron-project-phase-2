167c85a58945298e16d0f87a5bd439584c7cd170
RicardoUy <rvuy62@yahoo.com>
2023-09-17 17:23:16 -0500

    add search

--- src/components/Administrator/EmployeePage.js
+++ src/components/Administrator/EmployeePage.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import EmployeeList from './EmployeeList'
 import NewEmployeeForm from './NewEmployeeForm'
+import SearchEmployee from './SearchEmployee'
 
 const EmployeePage = () => {
     const [employees, setEmployees] = useState([])
+    const [searchTerm, setSearchTerm] = useState("")
 
     useEffect(() => {
         fetch("http://localhost:3001/employees")
@@ -14,13 +16,18 @@ const EmployeePage = () => {
           function handleAddEmployee (newEmployee) {
             setEmployees([...employees, newEmployee])
             }  
+
+  const filteredEmployees = employees.filter(employee => employee.name.toLowerCase().includes(searchTerm.toLowerCase()))          
+  
   return (
     <main>
       <div>
-        <EmployeeList employees={employees}/>
+        <EmployeeList employees={filteredEmployees}/>
         </div>
         <div id="employeeForm">
+        <SearchEmployee searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         <NewEmployeeForm onAddEmployee={handleAddEmployee}/>
+         
         </div>
         </main>
   )
